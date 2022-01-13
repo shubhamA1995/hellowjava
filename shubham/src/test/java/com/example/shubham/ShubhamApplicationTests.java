db312b401f9513167beb14421ac9722c6666fa3b
unknown <shubhamanantwar9418@gmail.com>
2022-01-13 17:34:50 +0530

    first

---
+++ shubham/src/test/java/com/example/shubham/ShubhamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shubham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShubhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
